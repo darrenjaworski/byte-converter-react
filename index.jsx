785ff22c5d365c4f85a375b5652d3ae2cf119454
Darren Jaworski <darrenjaworski@gmail.com>
2018-12-26 19:56:54 -0600

    Prettier, units, proptypes and defaultprops.

--- index.jsx
+++ index.jsx
@@ -4,8 +4,21 @@ import PropTypes from "prop-types";
 
 const BitConverter = () => {};
 
-BitConverter.PropTypes = {};
+const units = ["b", "B", "KB", "MB", "GB", "TB", "PB"];
 
-BitConverter.defaultProps = {};
+BitConverter.PropTypes = {
+  Children: PropTypes.number.isRequired,
+  hideWarnings: PropTypes.bool,
+  useSI: PropTypes.bool,
+  inUnit: PropTypes.oneOf(units),
+  outUnit: PropTypes.oneOf(units)
+};
+
+BitConverter.defaultProps = {
+  hideWarnings: false,
+  useSI: false,
+  inUnit: units[1],
+  outUnit: units[3]
+};
 
 export default BitConverter;
