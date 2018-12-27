de617c09948b40c47a9d517180b8cb23d67f4c5c
Darren Jaworski <darrenjaworski@gmail.com>
2018-12-26 19:21:20 -0600

    Added dev dependencies. Sketched basic component.

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+const BitConverter = () => {};
+
+BitConverter.PropTypes = {};
+
+BitConverter.defaultProps = {};
+
+export default BitConverter;
