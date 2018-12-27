0d8328318865a501880bb9cce2e795175351d21f
Darren Jaworski <darrenjaworski@gmail.com>
2018-12-26 20:55:09 -0600

    Webpack build. Name to match repo.

--- index.jsx
+++ index.jsx
@@ -1,24 +1,48 @@
 import React from "react";
-
-import PropTypes from "prop-types";
-
-const BitConverter = () => {};
+import propTypes from "prop-types";
 
 const units = ["b", "B", "KB", "MB", "GB", "TB", "PB"];
-
-BitConverter.PropTypes = {
-  Children: PropTypes.number.isRequired,
-  hideWarnings: PropTypes.bool,
-  useSI: PropTypes.bool,
-  inUnit: PropTypes.oneOf(units),
-  outUnit: PropTypes.oneOf(units)
+const mapUnitsToPower = {
+  B: 0,
+  KB: 1,
+  MB: 2,
+  GB: 3,
+  TB: 4,
+  PB: 5
 };
 
-BitConverter.defaultProps = {
+const ByteConverter = props => {
+  const { children, hideWarnings, useSI, inUnit, outUnit } = props;
+
+  // warn if children is integer or negative
+
+  return <>{children}</>;
+};
+
+ByteConverter.propTypes = {
+  children: propTypes.number.isRequired,
+  hideWarnings: propTypes.bool,
+  useSI: propTypes.bool,
+  inUnit: propTypes.oneOf(units),
+  outUnit: propTypes.oneOf(units)
+};
+
+ByteConverter.defaultProps = {
   hideWarnings: false,
   useSI: false,
   inUnit: units[1],
   outUnit: units[3]
 };
 
-export default BitConverter;
+export default ByteConverter;
+
+// - Children should always be a number. Warn on negative and noninteger values.  Suppress warnings with a prop.
+
+// - Ability to specify input units (default "B" for bytes)
+
+// - Ability to specify output units. Default "*iB" for auto-binary-prefixed Bytes.
+
+// Let's say for some reason I have total bits and I want to output as auto-metric-prefixed Bytes:
+
+// inUnits="b"
+// outUnits="*B"
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "byteConverter.js",
+    library: "byte-converter",
+    libraryTarget: "umd"
+  },
+  externals: ["react", "react-dom"],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/react"]
+        }
+      }
+    ]
+  }
+};
